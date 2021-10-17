07c9946f68e826195ec7e89853bf96798c335247
murtazaaylak <murtazaaylak@gmail.com>
2021-10-17 22:29:03 +0300

    language files added for footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,24 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer>
+      <div className='footer-inner-container'>
+        <div className='footer-upside'>
+          <div className='footer-logo-container'></div>
+          <div className='footer-company'></div>
+          <div className='footer-communities'></div>
+          <div className='footer-useful-links'></div>
+          <div className='footer-social-icons-container'></div>
+        </div>
+        <div className='footer-country'></div>
+        <div className='footer-bottom'>
+          <div className='footer-bottom-links'></div>
+          <div className='footer-copyright'></div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
