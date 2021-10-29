249b555b9aefb7950fb3435ac3ad66f9871dae58
murtazaaylak <murtazaaylak@gmail.com>
2021-10-29 16:48:51 +0300

    add: redux-persist to handle page-refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Routes from './Routes'
-import store from './redux/store'
+import { store } from './redux/store'
 import { Provider } from 'react-redux'
 
 const App = () => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,18 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
 
-const store = createStore(
-  rootReducer,
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk))
 )
-
-export default store
+export const persistor = persistStore(store)
