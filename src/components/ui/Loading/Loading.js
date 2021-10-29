49ab963f63034e3cf106fafc3757fb2023eb19b5
murtazaaylak <murtazaaylak@gmail.com>
2021-10-29 15:48:55 +0300

    add new component: collection page & loading

--- src/components/ui/Loading/Loading.js
+++ src/components/ui/Loading/Loading.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import './Loading.css'
 const Loading = () => {
+  const { t } = useTranslation()
   return (
     <div className='loading-container'>
-      <h1>Loading...</h1>
+      <h1>{t('loading')}</h1>
     </div>
   )
 }
--- src/pages/CollectionPage.js
+++ src/pages/CollectionPage.js
@@ -1,21 +1,59 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import VerticalCard from '../components/cards/VerticalCard'
 import GridContainer from '../components/GridContainer'
 import LeftNavbar from '../components/LeftNavbar/LeftNavbar'
+import { useSelector } from 'react-redux'
+import WebPlayer from '../components/WebPlayer'
+import { useTranslation } from 'react-i18next'
 
 const CollectionPage = ({ match }) => {
-  console.log(match)
+  const { t } = useTranslation('translation', { keyPrefix: 'collections' })
+  const playlists = useSelector((state) => state.playlists.list)
+  const podcasts = []
+  const artists = useSelector((state) => state.topArtists.list)
+  const albums = []
+  let list
+  switch (match.params.type) {
+    case 'playlists':
+      list = playlists
+      break
+    case 'podcasts':
+      list = podcasts
+      break
+    case 'artists':
+      list = artists
+      break
+    case 'albums':
+      list = albums
+      break
+    default:
+      list = []
+  }
+
   return (
     <div className='page-wrapper'>
       <LeftNavbar />
       <div className='page-content'>
-        <Banner
-          includeCollectionNav
-          activeTab='playlist'
-          activeTab={match.params.type}
-        />
-        <GridContainer></GridContainer>
+        <Banner includeCollectionNav activeTab={match.params.type} />
+        <GridContainer>
+          <div>
+            <h2>{t(`${match.params.type}`)}</h2>
+            <div className='demo-card-container'>
+              {list.map((playlist, i) => (
+                <React.Fragment key={i}>
+                  <VerticalCard
+                    data={playlist}
+                    isArtist={match.params.type === 'artists'}
+                    isPlaylist={match.params.type === 'playlists'}
+                  />
+                </React.Fragment>
+              ))}
+            </div>
+          </div>
+        </GridContainer>
       </div>
+      <WebPlayer />
     </div>
   )
 }
