101aeacaccc81de28dbc97c3146bf2aedb1674da
murtazaaylak <murtazaaylak@gmail.com>
2021-11-26 13:03:45 +0300

    refactor: horizontalcard class & css into scss

--- src/components/cards/HorizontalCard/HorizontalCard.js
+++ src/components/cards/HorizontalCard/HorizontalCard.js
@@ -3,25 +3,25 @@ import React, { useState } from 'react'
 import PauseIcon from '../../icons/PauseIcon'
 import PlayIcon from '../../icons/PlayIcon'
 
-import './HorizontalCard.css'
+import './horizontal-card.scss'
 
 const HorizontalCard = ({ playlistData }) => {
   const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className='horizontal-card'>
-      <div className='horizontal-card-inner-container'>
+      <div className='horizontal-card__inner'>
         {/* IMG */}
-        <div className='horizontal-card-img-container'>
+        <div className='horizontal-card__imgbox'>
           <img src={playlistData.images[0].url} alt={playlistData.name} />
         </div>
         {/* TITLE */}
-        <div className='horizontal-card-title'>
+        <div className='horizontal-card__title'>
           <p>{playlistData.name}</p>
         </div>
       </div>
       {/* ABSOLUTE POSITIONED PLAY BTN */}
       <div
-        className='horizontal-play-pause'
+        className='horizontal-card__play'
         onClick={() => setIsPlaying(!isPlaying)}
       >
         {isPlaying ? <PauseIcon /> : <PlayIcon />}
