b18d8f60dfd71a975611d0761fe25cf5053f8b1e
murtazaaylak <murtazaaylak@gmail.com>
2021-11-27 23:12:16 +0300

    refactor: playlistsongcard class & css into scss

--- src/components/cards/PlaylistSongCard/PlaylistSongCard.js
+++ src/components/cards/PlaylistSongCard/PlaylistSongCard.js
@@ -1,37 +1,39 @@
 import React from 'react'
 
-import './PlaylistSongCard.css'
+import './playlist-song-card.scss'
 
 const PlaylistSongCard = ({ songData, index }) => {
   return (
-    <div className='playlist-song-card'>
-      <div className='playlist-songs-table'>
+    <div className='playlist-card'>
+      <div className='playlist-card__table'>
         {/* ORDER NUM */}
-        <div className='playlist-songs-order playlist-songs-table-item'>
-          <p className='demo'>{index + 1}</p>
+        <div className='playlist-card__order playlist-card__table-item'>
+          <p>{index + 1}</p>
         </div>
 
-        <div className='playlist-songs-title playlist-songs-table-item'>
+        <div className='playlist-card__title playlist-card__table-item'>
           {/* SONG IMG */}
-          <div className='playlist-songs-title-img-container'>
+          <div className='playlist-card__imgbox'>
             <img
               src={songData.track?.album.images[1].url}
               alt={songData.name}
-              className='playlist-songs-title-img'
+              className='playlist-card__imgbox--img'
             />
           </div>
-          <div className='playlist-songs-title-detail'>
+          <div className='playlist-card__detail'>
             {/* SONG TITLE */}
-            <p className='playlist-song-name'>{songData.track?.name}</p>
+            <p className='playlist-card__detail--song-name'>
+              {songData.track?.name}
+            </p>
             {/* ARTIST */}
-            <p className='playlist-song-artistt'>
+            <p className='playlist-card__detail--artist'>
               {songData.track?.artists.map((artist, i) => {
                 {
                   /* if first artist leave it */
                 }
                 if (i === 0) {
                   return (
-                    <span className='playlist-song-artist' key={i}>
+                    <span className='playlist-card__detail--artist' key={i}>
                       {artist.name}{' '}
                     </span>
                   )
@@ -42,7 +44,7 @@ const PlaylistSongCard = ({ songData, index }) => {
                   return (
                     <span>
                       ,{' '}
-                      <span className='playlist-song-artist' key={i}>
+                      <span className='playlist-card__detail--artist' key={i}>
                         {artist.name}
                       </span>
                     </span>
@@ -53,15 +55,15 @@ const PlaylistSongCard = ({ songData, index }) => {
           </div>
         </div>
         {/* ALBUM NAME */}
-        <p className='playlist-songs-album playlist-songs-table-item'>
+        <p className='playlist-card__album playlist-card__table-item'>
           {songData.track?.name}
         </p>
         {/* ADDED AT */}
-        <div className='playlist-songs-added-on playlist-songs-table-item'>
+        <div className='playlist-card__added-on playlist-card__table-item'>
           {new Date(songData.added_at).toDateString()}
         </div>
         {/* DURATION */}
-        <div className='playlist-songs-duration playlist-songs-table-item'>
+        <div className='playlist-card__duration playlist-card__table-item'>
           3:44
         </div>
       </div>
