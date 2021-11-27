a15b3d562d1f17e632e562516316cb3bf25efa83
murtazaaylak <murtazaaylak@gmail.com>
2021-11-27 22:09:57 +0300

    refactor: musictypes card class & css into scss

--- src/components/cards/MusicTypesCard/MusicTypesCard.js
+++ src/components/cards/MusicTypesCard/MusicTypesCard.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-import './MusicTypesCard.css'
+import './music-types-card.scss'
 
 const MusicTypesCard = ({ category }) => {
   // TO GIVE RANDOM BG-COLOR
   const randomNum = Math.floor(Math.random() * 16) + 1
   return (
     <div className={`music-types-card search-card-${randomNum}`}>
-      <div className='music-types-card-inner-container'>
+      <div className='music-types-card__inner'>
         {/* CATEGORY IMG */}
         <img
           src={category.icons[0].url}
           alt={category.name}
-          className='music-types-card-img'
+          className='music-types-card__img'
         />
         {/* CATEGORY TITLE */}
-        <h2 className='music-types-card-title'>{category.name}</h2>
+        <h2 className='music-types-card__title'>{category.name}</h2>
       </div>
     </div>
   )
