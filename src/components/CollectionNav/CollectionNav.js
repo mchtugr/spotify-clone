00c9a53b2833a7ee1563db25f4a31dd71a0943f7
murtazaaylak <murtazaaylak@gmail.com>
2021-11-28 22:05:15 +0300

    refactor: collection-nav class & css into scss

--- src/components/CollectionNav/CollectionNav.js
+++ src/components/CollectionNav/CollectionNav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
-import './CollectionNav.css'
+import './collection-nav.scss'
 
 const CollectionNav = ({ activeTab }) => {
   // to translate from language files
@@ -11,38 +11,38 @@ const CollectionNav = ({ activeTab }) => {
   return (
     <ul className='collection-nav'>
       <li
-        className={`collection-nav-item  ${
-          activeTab === 'playlists' && 'collection-nav-selected'
+        className={`collection-nav__item  ${
+          activeTab === 'playlists' && 'collection-nav__item--selected'
         }`}
       >
-        <Link to='/collection/playlists' className='collection-nav-link'>
+        <Link to='/collection/playlists' className='collection-nav__link'>
           {t('playlists')}
         </Link>
       </li>
       <li
-        className={`collection-nav-item  ${
-          activeTab === 'podcasts' && 'collection-nav-selected'
+        className={`collection-nav__item  ${
+          activeTab === 'podcasts' && 'collection-nav__item--selected'
         }`}
       >
-        <Link to='/collection/podcasts' className='collection-nav-link'>
+        <Link to='/collection/podcasts' className='collection-nav__link'>
           {t('podcasts')}
         </Link>
       </li>
       <li
-        className={`collection-nav-item  ${
-          activeTab === 'artists' && 'collection-nav-selected'
+        className={`collection-nav__item  ${
+          activeTab === 'artists' && 'collection-nav__item--selected'
         }`}
       >
-        <Link to='/collection/artists' className='collection-nav-link'>
+        <Link to='/collection/artists' className='collection-nav__link'>
           {t('artists')}
         </Link>
       </li>
       <li
-        className={`collection-nav-item  ${
-          activeTab === 'albums' && 'collection-nav-selected'
+        className={`collection-nav__item  ${
+          activeTab === 'albums' && 'collection-nav__item--selected'
         }`}
       >
-        <Link to='/collection/albums' className='collection-nav-link'>
+        <Link to='/collection/albums' className='collection-nav__link'>
           {t('albums')}
         </Link>
       </li>
