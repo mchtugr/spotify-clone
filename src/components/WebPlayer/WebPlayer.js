f6d7b63a1aff6196efe3f159f9ac422b6274f959
murtazaaylak <murtazaaylak@gmail.com>
2021-11-30 22:28:40 +0300

    refactor: web player class & css into scss

--- src/components/WebPlayer/WebPlayer.js
+++ src/components/WebPlayer/WebPlayer.js
@@ -13,7 +13,7 @@ import ConnectToDeviceIcon from '../icons/ConnectToDeviceIcon'
 import VolumeHighIcon from '../icons/VolumeHighIcon'
 import FullScreenIcon from '../icons/FullScreenIcon'
 
-import './WebPlayer.css'
+import './web-player.scss'
 
 const WebPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false)
@@ -24,59 +24,58 @@ const WebPlayer = () => {
       <div className='web-player'>
         {/* LEFT */}
         <div className='player-left'>
-          <div className='footer-img-container'>
+          <div className='player-left__imgbox'>
             <img
-              src='https://raw.githubusercontent.com/murtazaaylak/spotify-clone/main/public/deneme.jpeg'
+              src='https://raw.githubusercontent.com/mchtugr/spotify-clone/main/public/deneme.jpeg'
               alt='song-poster'
-              className='footer-img'
+              className='player-left__img'
             />
           </div>
-          <div className='footer-song-details'>
-            <p className='footer-song-title'>MargaM</p>
-            <p className='footer-artist'>
+          <div className='player-left__song'>
+            <p className='player-left__song--title'>MargaM</p>
+            <p className='player-left__song--artist'>
               <small>Çetin Çetintaş</small>
             </p>
           </div>
           <div
-            className={`footer-like footer-icons ${isFav && 'liked-icon'}`}
+            className={`footer-like web-player__icons ${isFav && 'liked-icon'}`}
             onClick={() => setIsFav(!isFav)}
           >
             <FooterLikeIcon />
           </div>
-          <div className='footer-poster footer-icons'>
+          <div className='footer-poster web-player__icons'>
             <ShowPosterIcon />
           </div>
         </div>
         {/* MIDDLE */}
         <div className='player-middle'>
-          <div className='player-middle-container'>
-            <div className='player-control'>
-              <div className='left-control'>
-                <div className='shuffle-icon footer-icons'>
+          <div className='player-middle__inner'>
+            <div className='player-middle__control'>
+              <div className='player-middle__control--left'>
+                <div className='web-player__icons'>
                   <ShuffleIcon />
                 </div>
-                <div className='prev-song-icon footer-icons'>
+                <div className='web-player__icons'>
                   <PrevSongIcon />
                 </div>
               </div>
               <div
-                className='play-pause-btn'
+                className='web-player__play-btn'
                 onClick={() => setIsPlaying(!isPlaying)}
               >
                 {isPlaying ? <PauseIcon /> : <PlayIcon />}
               </div>
-              {/* RIGHT */}
-              <div className='right-control'>
-                <div className='next-song-icon footer-icons'>
+              <div className='player-middle__control--right'>
+                <div className='web-player__icons'>
                   <NextSongIcon />
                 </div>
-                <div className='repeat-icon footer-icons'>
+                <div className='repeat-icon web-player__icons'>
                   <RepeatIcon />
                 </div>
               </div>
             </div>
-            <div className='playback-bar'>
-              <p className='current-time'>0:30</p>
+            <div className='player-middle__playback-bar'>
+              <p className='player-middle__playback-bar--time'>0:30</p>
               <input
                 type='range'
                 name=''
@@ -85,21 +84,22 @@ const WebPlayer = () => {
                 step='0.01'
                 // value='199'
                 max='200'
-                className='duration-bar'
+                className='web-player__playback-bar'
               />
-              <p className='total-time'>3:50</p>
+              <p className='player-middle__playback-bar--time'>3:50</p>
             </div>
           </div>
         </div>
+        {/* RIGHT */}
         <div className='player-right'>
-          <div className='footer-queue footer-icons'>
+          <div className='web-player__icons'>
             <QueueIcon />
           </div>
-          <div className='device-connect footer-icons'>
+          <div className='web-player__icons'>
             <ConnectToDeviceIcon />
           </div>
-          <div className='sound-control'>
-            <div className='footer-icons'>
+          <div className='player-right__sound'>
+            <div className='web-player__icons'>
               <VolumeHighIcon />
             </div>
             <input
@@ -110,10 +110,10 @@ const WebPlayer = () => {
               step='0.01'
               // value='199'
               max='200'
-              className='duration-bar'
+              className='web-player__playback-bar'
             />
           </div>
-          <div className='full-screen footer-icons'>
+          <div className='web-player__icons'>
             <FullScreenIcon />
           </div>
         </div>
